78b7f9bc08250b61df6fa24b673cd16790b7ea6a
dmongiello <dmongiello@yahoo.com>
2016-10-31 11:56:21 -0700

    initial commit

---
+++ src/FinalProject/FinalProject.java
@@ -0,0 +1,109 @@
+/***************************************************************
+* file: FinalProject.java
+* authors: D. Mongiello
+* class: CS 445 Computer Graphics
+*
+* assignment: Final Project Checkpoint 1
+* date last modified: 10/31/2016 *
+* purpose: This program creates a cube in 3d spaces and allows the user 
+* to move around in 3d space while viewing the cube. 
+* */
+package FinalProject;
+
+/**
+ *
+ * @author David R. Mongiello
+ *         
+ */
+import org.lwjgl.input.Keyboard; 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode; 
+import static org.lwjgl.opengl.GL11.*; 
+import org.lwjgl.util.glu.GLU;
+
+public class FinalProject { 
+private FPCameraController fp = new FPCameraController(0f,0f,0f);
+private DisplayMode displayMode;
+// method: start()
+// purpose: This method creates an instance of the class allowing for use of
+// object oriented programming. It removes the static of main. And is a virtual 
+// instance of main
+    public void start()
+    {
+    try {
+        createWindow(); initGL();
+        fp.gameLoop();//render(); 
+    } catch (Exception e) {
+        e.printStackTrace(); }
+    }
+    
+// method: createWindow
+// purpose: This method creates a window with openGL and setup the display area. 
+    private void createWindow() throws Exception
+    {
+       Display.setFullscreen(false);
+        DisplayMode d[] = Display.getAvailableDisplayModes();
+        for (int i = 0; i < d.length; i++) 
+        { 
+            if (d[i].getWidth() == 640 && d[i].getHeight() == 480
+                && d[i].getBitsPerPixel() == 32) 
+            { 
+                displayMode = d[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode); 
+        Display.setTitle("Final Project"); 
+        Display.create();
+    }
+
+// method: initGL()
+// purpose: This method initializes the GL screen. 
+    private void initGL() {
+        // Background color black
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glMatrixMode(GL_PROJECTION); 
+        glLoadIdentity();
+        // Clipping Area 
+        GLU.gluPerspective(100.0f, (float)displayMode.getWidth()/(float) displayMode.getHeight(), 0.1f, 300.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); 
+    }
+    
+// method: render()
+// purpose: This method renders the models as they change.
+    private void render() {
+        //For color changing code
+        
+        while (!Display.isCloseRequested()) {
+            // Check if esc is being pressed
+            if (!Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
+            {
+            
+            try
+            {    
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); glLoadIdentity();
+           
+               
+            
+            }catch(Exception e){ }
+            }
+            else {
+                // Destroy and exit program if esc is pressed
+                Display.destroy();
+                System.exit(0);
+            }
+            } Display.destroy();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        FinalProject p1 = new FinalProject();
+        p1.start(); 
+        
+    }
+    
+}
---
+++ src/FinalProject/Vector3Float.java
@@ -0,0 +1,22 @@
+/***************************************************************
+* file: Vector3Float.java
+* authors: D. Mongiello
+* class: CS 445 Computer Graphics
+*
+* assignment: Final Project Checkpoint 1
+* date last modified: 10/31/2016 *
+* purpose: The purpose of this class is to hold the camera's position 
+* in 3d space 
+* */
+package FinalProject;
+
+/**
+ *
+ * @author davidmongiello
+ */
+public class Vector3Float {
+ public float x, y, z;
+public Vector3Float(int x, int y, int z){
+this.x=x; this.y=y; this.z=z;
+}   
+}
